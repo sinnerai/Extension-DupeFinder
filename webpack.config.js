84bd1645bf95c267b9dca4fd7965704b5f190172
sinnerai <163897656+sinnerai@users.noreply.github.com>
2024-03-18 18:37:20 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        path: path.join(__dirname, 'dist/'),
+        filename: `index.js`,
+        publicPath: '/scripts/extensions/third-party/Extension-DupeFinder/dist/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                options: {
+                    cacheDirectory: true,
+                    presets: [
+                        '@babel/preset-env',
+                        ['@babel/preset-react', { runtime: 'automatic' }],
+                    ],
+                },
+                loader: 'babel-loader',
+            },
+        ],
+    },
+    optimization: {
+        minimize: false,
+        minimizer: [new TerserPlugin({
+            extractComments: false,
+        })],
+    },
+};
